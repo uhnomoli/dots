29aa3decfabf066143e932e7960bd17e9a29d8e8
Andrew Fricke <160705+uhnomoli@users.noreply.github.com>
2019-03-03 05:39:03 -0500

    Add ZSH function for configuring web cam

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,3 +1,18 @@
+webcam_reset() {
+    v4l2-ctl -c brightness=128
+    v4l2-ctl -c contrast=128
+    v4l2-ctl -c saturation=128
+    v4l2-ctl -c white_balance_temperature_auto=0
+    v4l2-ctl -c gain=25
+    v4l2-ctl -c white_balance_temperature=4600
+    v4l2-ctl -c sharpness=128
+    v4l2-ctl -c exposure_auto=1
+    v4l2-ctl -c exposure_absolute=450
+    v4l2-ctl -c exposure_auto_priority=0
+    v4l2-ctl -c focus_auto=0
+    v4l2-ctl -c focus_absolute=0
+}
+
 pcd() {
     if [ $# -eq 1 ] && [[ "$1" =~ "^[a-zA-Z0-9]+$" ]]; then
         local dst="${HOME}/projects/python/${1}"
